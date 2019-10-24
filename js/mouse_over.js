da6fc0e99de6a3ef37442cdebad54013f6736e07
Jordan Sutton <jordan.sutton@hpe.com>
2019-10-24 12:53:51 -0600

    Using a premade bootstrap profile

--- js/mouse_over.js
+++
@@ -1,6 +0,0 @@
-
-$('#avatar').hover(function() {
-  $(this).attr('src', 'https://twitter.com/jsutcodes/profile_image?size=original');
-}, function() {
-  $(this).attr('src', 'https://avatars.githubusercontent.com/jsutcodes');
-});
